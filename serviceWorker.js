138c791c17dcf8cf88d51d967b2657b94cf32341
Gustavo Monjardim <gustavo.monjardim@liftbank.com.br>
2020-04-22 22:38:34 -0300

    feat: adiciona push handler no serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,1 +1,12 @@
-console.log('My service worker');
+self.addEventListener('push', (event) => {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'My notification';
+  const options = {
+    body: event.data.text(),
+    vibrate: [200, 100, 200, 100, 200, 100, 200],
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
