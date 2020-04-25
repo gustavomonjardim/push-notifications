355bf3454a19aab86633d69a99ee0211df33ebf1
Gustavo Monjardim <gustavo.monjardim@liftbank.com.br>
2020-04-25 15:23:36 -0300

    feat: adiciona lambda function do netlify

---
+++ src/functions/push-service.js
@@ -0,0 +1,15 @@
+export async function handler(event) {
+  console.log(event);
+  try {
+    const subject = event.queryStringParameters.name || 'World';
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    };
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() };
+  }
+}
